7864c56ffd1ca11be9fdfb6a17ab810d8df4121f
Bitl <toycrazy@hotmail.com>
2017-06-21 13:03:00 -0700

    1.18 2

--- RBXLegacyLauncher/RBXLegacyLauncher/SecurityFuncs.cs
+++ RBXLegacyLauncher/RBXLegacyLauncher/SecurityFuncs.cs
@@ -86,7 +86,7 @@ namespace RBXLegacyLauncher
 		
 		public static bool checkScriptMD5()
 		{
-			string rbxexe = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\scripts\\CSMPFunctions.exe";
+			string rbxexe = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\scripts\\CSMPFunctions.lua";
     		using (var md5 = MD5.Create())
     		{
     			using (var stream = File.OpenRead(rbxexe))
