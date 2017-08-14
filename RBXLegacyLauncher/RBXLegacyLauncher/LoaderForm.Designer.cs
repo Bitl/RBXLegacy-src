5e7a7e74ffa0d5b615cdfabaafbe616f36da6353
Quacky <puzzledog121@gmail.com>
2017-08-13 22:47:07 -0500

    old roblox loading

--- RBXLegacyLauncher/RBXLegacyLauncher/LoaderForm.Designer.cs
+++ RBXLegacyLauncher/RBXLegacyLauncher/LoaderForm.Designer.cs
@@ -27,36 +27,37 @@
 			// 
 			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(163, 29);
+			this.pictureBox1.Location = new System.Drawing.Point(17, 18);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(130, 109);
+			this.pictureBox1.Size = new System.Drawing.Size(52, 51);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(12, 187);
+			this.progressBar1.Location = new System.Drawing.Point(79, 49);
 			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(429, 28);
+			this.progressBar1.Size = new System.Drawing.Size(266, 27);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
 			this.progressBar1.TabIndex = 1;
 			// 
 			// label1
 			// 
-			this.label1.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(105, 153);
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 9.25F);
+			this.label1.Location = new System.Drawing.Point(76, 21);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(260, 31);
+			this.label1.Size = new System.Drawing.Size(270, 19);
 			this.label1.TabIndex = 2;
-			this.label1.Text = "Initializing...";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label1.Text = "Static";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// button1
 			// 
-			this.button1.Font = new System.Drawing.Font("Source Sans Pro", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(163, 221);
+			this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(257, 81);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(130, 30);
+			this.button1.Size = new System.Drawing.Size(88, 30);
 			this.button1.TabIndex = 3;
 			this.button1.Text = "Cancel";
 			this.button1.UseVisualStyleBackColor = true;
@@ -65,16 +66,17 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-			this.ClientSize = new System.Drawing.Size(453, 254);
-			this.ControlBox = false;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(358, 132);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.pictureBox1);
+			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "LoaderForm";
+			this.Text = "RBXLegacy";
 			this.Load += new System.EventHandler(this.LoaderFormLoad);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
